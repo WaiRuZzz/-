709caa2fa3fe6c8e80111cf1a593d2b25c734ff0
wairuzzz <wairuzzz@yandex.ru>
2022-08-09 23:23:24 +0300

    DZ

---
+++ 5/Program.cs
@@ -0,0 +1,47 @@
+﻿void FillArray(int[] Collection)
+{
+    int lentch = Collection.Length;
+    int index = 0;
+    while (index < lentch)
+    {
+        Collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] Collection, int find)
+{
+    int count = Collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if (Collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+int pos = IndexOf(array,9);
+Console.WriteLine(pos);
+
