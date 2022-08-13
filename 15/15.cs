ccba273cf22fd7e9b98cefecb2ba59bf96aa8730
wairuzzz <wairuzzz@yandex.ru>
2022-08-13 22:33:49 +0300

    two

---
+++ 15/15.cs
@@ -0,0 +1,20 @@
+﻿int a;
+int b;
+int c;
+a=1;
+b=6;
+c=7;
+Console.WriteLine("введите  число 1-7: ");
+a = Convert.ToInt32(Console.ReadLine());
+if (a < 6)
+{
+Console.WriteLine($" {a} будний день");
+}
+else if (a == 6)
+{
+    Console.WriteLine($" {b} выходной день");
+}
+else if (a == 7)
+{
+    Console.WriteLine($" {c} выходной день");
+}
