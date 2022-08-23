3fdd792461f23bd1b35534ab5dbd8e87e32b5aed
wairuzzz <wairuzzz@yandex.ru>
2022-08-23 08:11:51 +0300

    DZZ

---
+++ 27/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.WriteLine("введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int DigitCount(int num)
+{
+    int result = 0;
+
+    result = num % 10;
+    num = num / 10;
+    if (num > 0)
+    {
+        result = result + DigitCount(num);
+    }
+    return result;
+}
+Console.WriteLine(DigitCount(num));
